5a7278d2c5de56e9976200afd52ffbb933351c4e
mh6497 <hoangminhhungcvp@gmail.com>
2020-03-17 12:26:04 +0700

    Change app name procfile

--- locallibrary/locallibrary/wsgi.py
+++ locallibrary/locallibrary/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.locallibrary.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locallibrary.locallibrary.settings')
 
 application = get_wsgi_application()
 
